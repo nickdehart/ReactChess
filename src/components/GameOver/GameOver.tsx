9a5d8efa8488de3443393594f2dbf8c9e25fb518
Nicholas DeHart <nickdehart@gmail.com>
2024-05-10 14:35:29 -0400

    Updated gameover to use modal component

--- src/components/GameOver/GameOver.tsx
+++ src/components/GameOver/GameOver.tsx
@@ -1,15 +1,18 @@
 import classes from './GameOver.module.css';
 import { Teams } from '@/types/enums';
+import { Modal } from "@/components/Modal";
 
 
 export function GameOver({ gameOver, turn }: { gameOver: boolean, turn: Teams }) {
 
     if(gameOver)
         return (
-            <div className={classes.gameOver}>
-                <h1>GAME OVER</h1>
-                <h4>{turn === Teams.WHITE ? "Black" : "White"} Team Wins!</h4>
-            </div>
+            <Modal open={gameOver} header={false} closable={false} maskClosable={false} footer={false} modalClassName={classes.gameOverModal}>
+                <div className={classes.gameOver}>
+                    <h1>GAME OVER</h1>
+                    <h2>{turn === Teams.WHITE ? "Black" : "White"} Team Wins!</h2>
+                </div>
+            </Modal>
         );
     return <></>;
 }
