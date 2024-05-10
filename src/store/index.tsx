2223a460c57811b4fba5b8b3c7c7325336815780
Nicholas DeHart <nickdehart@gmail.com>
2024-05-10 14:37:26 -0400

    Added game store provider to Store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from "react";
+import { GameStore } from "./game";
 import { BoardStore } from "./board";
 
-const providers = [BoardStore.Provider];
+const providers = [GameStore.Provider, BoardStore.Provider];
 
 interface StoreProps {
     children: ReactNode,
