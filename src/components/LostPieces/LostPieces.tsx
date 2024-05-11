33c476ffcb14cb69400ad79ae3e031ce3c77728b
Nicholas DeHart <nickdehart@gmail.com>
2024-05-11 16:58:27 -0400

    reworking the lostpieces component to make more sense

--- src/components/LostPieces/LostPieces.tsx
+++ src/components/LostPieces/LostPieces.tsx
@@ -1,17 +1,36 @@
 import { useId } from 'react';
 import classes from './LostPieces.module.css';
+import { useGame } from "@/hooks/useGame";
 import { Cell } from '@/types/common';
 import { getImage } from "@/utilities/imageUtils";
 
 
-export function LostPieces({ pieces }: { pieces: Cell[] }) {
+export function LostPieces() {
     const id = useId();
+    const { game } = useGame();
+    const { lostWhitePieces, lostBlackPieces } = game;
+
+    const mapPieces = (pieces: Cell[]) => {
+        return pieces.map(({ team, type }: Cell, index: number) => 
+            <img 
+                key={`${id}-lost-white-${index}`} 
+                className={classes.img} 
+                src={getImage(type, team)} 
+                alt={`${team} ${type}`} 
+            />
+        )
+    }
 
     return (
-        <span className={classes.span}>
-            {pieces.map(({ team, type }: Cell, index: number) => 
-                <img key={`${id}-lost-${index}`} className={classes.img} src={getImage(type, team)} alt={`${team} ${type}`} />
-            )}
-        </span>
+        <aside className={classes.lostPiecesContainer}>
+            <div className={classes.teamContainer}>
+                <b>Taken White Pieces</b>
+                <span className={classes.imageContainer}>{mapPieces(lostWhitePieces)}</span>
+            </div>
+            <div className={classes.teamContainer}>
+                <b>Taken Black Pieces</b>
+                <span className={classes.imageContainer}>{mapPieces(lostBlackPieces)}</span>
+            </div>
+        </aside>
     );
 }
