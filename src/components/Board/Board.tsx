8e336d8dce714094c0a09d781631a2349b178902
Nicholas DeHart <nickdehart@gmail.com>
2024-05-11 16:59:32 -0400

    separated some logic out of Board into a new Game component

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,114 +1,37 @@
-import { useState }        from "react";
-import classes             from './Board.module.css';
-
-import { Piece }           from "@/components/Piece";
-import { LostPieces }      from "@/components/LostPieces";
-import { GameOver }        from "@/components/GameOver";
-import { PromotionModal }  from "@/components/PromotionModal";
-import { useBoard }        from "@/hooks/useBoard";
-import { useGame }         from "@/hooks/useGame";
-import { Cell }            from '@/types/common';
-import { Pieces, Teams, Columns, Rows } from '@/types/enums';
+import classes      from './Board.module.css';
+import { Piece }    from "@/components/Piece";
+import { useBoard } from "@/hooks/useBoard";
+import { Cell }     from '@/types/common';
 
 
-export function Board() {
-
-    const { board, castle, move } = useBoard();
-    const { game, setTarget, setLostWhitePieces, setLostBlackPieces, setGameOverStatus } = useGame();
-    const { turn, active, lostWhitePieces, lostBlackPieces, gameOverStatus } = game;
-    const [open, setOpen] = useState<boolean>(false);
-
-
-    function isCastleMove(target: Cell) :boolean {
-        if(!active) return false;
-        if(active.type !== Pieces.KING) return false;
-
-        // castle right
-        if(active.y === target.y && active.x === Columns.E && target.x === Columns.G)
-            return true;
-
-        // castle left
-        if(active.y === target.y && active.x === Columns.E && target.x === Columns.C) 
-            return true;
-
-        return false;
-    }
-
-
-    function isPromotion(target: Cell) :boolean {
-        if(!active) return false;
-        if(active.type !== Pieces.PAWN) return false;
-
-        if(active.team === Teams.WHITE && target.y === Rows.ONE)
-            return true;
-
-        if(active.team === Teams.BLACK && target.y === Rows.EIGHT)
-            return true;
-
-        return false;
-    }
-
-
-    function handleMove(target: Cell) {
-        // only an active piece can move
-        if(!active) return;
-
-        if (target.type === Pieces.KING) setGameOverStatus(true);
-
-        // Push lost pieces
-        if(target.team === Teams.WHITE) setLostWhitePieces(target);
-        if(target.team === Teams.BLACK) setLostBlackPieces(target);
-
-        // Update board and change turns
-        if(isCastleMove(target)) castle({ target, active });
-        else if(isPromotion(target)) {
-            setOpen(true);
-            setTarget(target);
-            return;
-        }
-        else move({ target, active });
-    };
-
+export function Board({ handleMove }: { handleMove: (target: Cell)=>void }) {
+    const { board } = useBoard();
 
     return (
-        <section className={classes.section}>
+        <table className={classes.table}>
+            <thead>
+                <tr><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th></tr>
+            </thead>
 
-            <GameOver gameOver={gameOverStatus} turn={turn} />
+            <tbody>
+                {board.map((row, rowIndex) => (
+                    <tr key={`row-${rowIndex}`}>
 
-            <LostPieces pieces={lostWhitePieces} />
+                        <td><b>{rowIndex + 1}</b></td>
 
-            <table className={classes.table}>
-                <thead>
-                    <tr><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th></tr>
-                </thead>
+                        {row.map((col, colIndex) => 
+                            <td
+                                key={`col-${colIndex}`}
+                                onClick={col.highlight ? () => handleMove(col) : () => {}}
+                                className={`${classes.cell} ${col.highlight ? classes.cellHighlight : classes.cellNoHighlight}`}
+                            >
+                                {col.type !== null && <Piece piece={col} />}
+                            </td>
+                        )}
 
-                <tbody>
-                    {board.map((row, rowIndex) => (
-                        <tr key={`row-${rowIndex}`}>
-
-                            <td><b>{rowIndex + 1}</b></td>
-
-                            {row.map((col, colIndex) => 
-                                <td
-                                    key={`col-${colIndex}`}
-                                    onClick={col.highlight ? () => handleMove(col) : () => {}}
-                                    className={`${classes.cell} ${col.highlight ? classes.cellHighlight : classes.cellNoHighlight}`}
-                                >
-                                    {col.type !== null && <Piece piece={col} />}
-                                </td>
-                            )}
-
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            <LostPieces pieces={lostBlackPieces} />
-
-            {/* <button className={classes.newGameBtn} onClick={handleReset}>Start New Game</button> */}
-
-            <PromotionModal open={open} setOpen={setOpen} />
-
-        </section>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,12 +2,13 @@ import classes from './Footer.module.css';
 import github from '@/assets/github.svg';
 
 export function Footer() {
+    const year = new Date().getFullYear();
+    const version = import.meta.env.VITE_GIT_INFO ? `${import.meta.env.VITE_GIT_INFO} |` : '';
     return (
         <footer className={classes.footer}>
             <a href="https://github.com/nickdehart/ReactChess" target="_blank" className={classes.footerLink}>
-                Nicholas DeHart
                 <span>&copy;</span> 
-                {new Date().getFullYear()}
+                {year} | Nicholas DeHart | {version}
                 <img src={github} alt="Github Icon" className={classes.gitgubIcon} />
             </a>
         </footer>
