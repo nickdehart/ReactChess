c4763c163276e6a83c398327627be699c0c167ac
Nicholas DeHart <nickdehart@gmail.com>
2024-06-22 15:53:56 -0400

    getting somewhere

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import github from '@/assets/github.svg';
 
 export function Footer() {
     const year = new Date().getFullYear();
-    const version = import.meta.env.VITE_GIT_INFO ? `${import.meta.env.VITE_GIT_INFO} |` : '';
-    const version_backup = `${import.meta.env.VITE_GITHUB_REF_NAME}-${import.meta.env.VITE_GITHUB_SHA}`
+    const version = import.meta.env.VITE_GITHUB_REF_NAME && import.meta.env.VITE_GITHUB_SHA ?
+        `${import.meta.env.VITE_GITHUB_REF_NAME}-v0.0.0-${import.meta.env.VITE_GITHUB_SHA.slice(0, 7)}` : "";
     return (
         <footer className={classes.footer}>
             <a href="https://github.com/nickdehart/ReactChess" target="_blank" className={classes.footerLink}>
                 <span>&copy;</span> 
-                {year} | Nicholas DeHart | {version || version_backup}
+                {year} | Nicholas DeHart | {version}
                 <img src={github} alt="Github Icon" className={classes.gitgubIcon} />
             </a>
         </footer>
