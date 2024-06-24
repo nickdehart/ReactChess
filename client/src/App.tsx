f6ac120d7b8c888d2384e9ef7fdf29f4927bc53d
Nicholas DeHart <nickdehart@gmail.com>
2024-06-24 12:23:13 -0400

    Moved Header to layout/ directory for static layout components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/Header';
+import { Header } from '@/components/layout/Header';
 import { Game } from '@/components/Game';
 import { Footer } from '@/components/layout/Footer';
 import { Store } from './store';
