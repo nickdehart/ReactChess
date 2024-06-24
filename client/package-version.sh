a7390d72f491b5d9471d6e35b94e3701b1f171a3
Nicholas DeHart <nickdehart@gmail.com>
2024-06-24 12:07:18 -0400

    Update package-version.sh

--- client/package-version.sh
+++ client/package-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-echo "Reading package.json from ./package.json"
-PACKAGE_VERSION=$(cat ./package.json | jq '.version' | tr -d '"')
+echo "Reading package.json from ./client/package.json"
+PACKAGE_VERSION=$(cat ./client/package.json | jq '.version' | tr -d '"')
 
 echo "current-version=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
